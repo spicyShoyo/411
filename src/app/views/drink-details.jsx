8fd6bdfe8b7a01e49f14a46f2a45b10c07aa6ab2
lafickens <yuxiaohang87878@msn.com>
2016-03-26 17:57:42 -0500

    Details dialog

--- src/app/views/drink-details.jsx
+++ src/app/views/drink-details.jsx
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import { Dialog, FlatButton } from 'material-ui/lib';
+import { Dialog, FlatButton, Tabs, Tab } from 'material-ui/lib';
 import { Card, CardMedia, CardTitle, CardText } from 'material-ui/lib/card';
 
 import UIDispatcher from './../utils/ui-dispatcher';
@@ -11,15 +11,18 @@ import UIEvents from './../utils/ui-events';
 
 const styles = {
   dialog: {
+    overflow: 'auto'
   },
-  cardMedia: {
-    maxHeight: '10%'
+  drinkSummary: {
+    float: 'left'
+  },
+  table: {
+    width: '100%'
   },
   div: {
     display: 'inline'
   },
   image: {
-    marginLeft: '60px',
     width: '300px',
     height : '300px',
     maxHeight: '100%',
@@ -45,21 +48,79 @@ export default React.createClass({
         label="Close"
         onTouchTap={() => UIDispatcher.emit(UIEvents.DRINK_DETAILS_TOGGLE)} />
     ];
+    const ingredientsRow = [];
+    this.props.ingredients.forEach(i => {
+      ingredientsRow.push(
+        <tr>
+          <td>{i.ingredientname}</td>
+          <td>{i.amount}</td>
+          <td>{i.unit}</td>
+        </tr>
+      );
+    });
+
     return (
+      //<Dialog
+      //  open={this.props.open}
+      //  actions={actions}
+      //  style={styles.dialog}>
+      //  <h1 style={styles.title}>{this.props.details.drinkname}</h1>
+      //  <img style={styles.image} src={this.props.details.url} />
+      //  <div style={styles.contentDiv}>
+      //    <h3>Category</h3>
+      //    <h5>{this.props.details.category}</h5>
+      //    <h3>Alcohol Type?</h3>
+      //    <h5>{this.props.details.alcoholic ? 'Alcoholic' : 'Shots'}</h5>
+      //    <h3>Glass</h3>
+      //    <h5>{this.props.details.glass}</h5>
+      //  </div>
+      //</Dialog>
       <Dialog
         open={this.props.open}
         actions={actions}
-        style={styles.dialog}>
-        <h1 style={styles.title}>{this.props.details.drinkname}</h1>
-        <img style={styles.image} src={this.props.details.url} />
-        <div style={styles.contentDiv}>
-          <h3>Category</h3>
-          <h5>{this.props.details.category}</h5>
-          <h3>Alcohol Type?</h3>
-          <h5>{this.props.details.alcoholic ? 'Alcoholic' : 'Shots'}</h5>
-          <h3>Glass</h3>
-          <h5>{this.props.details.glass}</h5>
-        </div>
+        bodyStyle={styles.dialog}>
+        <Card>
+          <CardMedia
+            overlay={<CardTitle title={this.props.details.drinkname}/>}>
+            <img style={styles.image} src={this.props.details.url}/>
+          </CardMedia>
+          <CardText>
+            <Tabs>
+              <Tab label="Summary">
+                <table style={styles.table}>
+                  <thead>
+                    <tr>
+                      <th><h3>Category</h3></th>
+                      <th><h3>Alcohol Type</h3></th>
+                      <th><h3>Glass</h3></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{this.props.details.category}</td>
+                      <td>{this.props.details.alcoholic ? 'Alcoholic' : 'Shots'}</td>
+                      <td>{this.props.details.glass}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Tab>
+              <Tab label="Ingredients">
+                <table style={styles.table}>
+                  <thead>
+                    <tr>
+                      <th><h3>Name</h3></th>
+                      <th><h3>Amount</h3></th>
+                      <th><h3>Unit</h3></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {ingredientsRow}
+                  </tbody>
+                </table>
+              </Tab>
+            </Tabs>
+          </CardText>
+        </Card>
       </Dialog>
     );
   }

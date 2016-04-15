9763fc18a6b5d188855e4dfc772ebb41eaf9319f
mliu-dark-knight <mengxiongliu@mengxiongliudeMacBook-Pro.local>
2016-04-14 23:59:23 -0500

    new drink card

--- src/app/views/create-drink-view.jsx
+++ src/app/views/create-drink-view.jsx
@@ -10,37 +10,21 @@ import api from '../api.jsx';
 import FloatingActionButton from 'material-ui/lib/floating-action-button';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import UserStore from '../stores/user-store';
-import { Card, CardMedia, CardActions, CardHeader} from 'material-ui/lib/card';
+import { Card, CardMedia, CardText, CardActions, CardHeader} from 'material-ui/lib/card';
 
 
 const styles = {
   dialog: {
     overflow: 'auto'
   },
+  table: {
+    width: '100%'
+  },
   image: {
     width: '300px',
     height : '300px',
     maxHeight: '100%',
     maxWidth: '100%'
-  },
-  container: {
-    width: '100%',
-    height: '50%',
-    position: 'relative',
-    width: '50%',
-    minWidth: 300,
-    maxWidth: 450,
-    minHeight: 400,
-    maxHeight: 1200,
-  },
-  backgroundMask: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: 200,
-    // backgroundImage: 'url("/Poly15.jpg")',
-    backgroundSize: '100% auto',
   },
   view: {
     background: 'transparent',
@@ -63,7 +47,7 @@ export default class CreateDrinkView extends React.Component {
     this.state = {
       open: false,
       url: "",
-      buttons: []};
+      ingredientsRow: []};
       this.handleOpen = this.handleOpen.bind(this);
       this.handleClose = this.handleClose.bind(this);
       this.createDrinkRequest = this.createDrinkRequest.bind(this);
@@ -79,20 +63,20 @@ export default class CreateDrinkView extends React.Component {
   }
 
   createDrinkRequest() {
-    this.state.buttons=[]
+    this.state.ingredientsRow=[]
     api.createDrink(UserStore.username).then(res => {
       let resArr = res["ingredients"];
       resArr.forEach(item => {
-        this.state.buttons.push(
-          <section>
-          <RaisedButton
-          label={item["ingredientname"]}
-          keyboardFocused={true}>
-          </RaisedButton>
-          </section>)
-        })
-        this.setState({buttons: this.state.buttons});
+        this.state.ingredientsRow.push(
+          <tr>
+            <td>{item.ingredientname}</td>
+            <td>{item.amount}</td>
+            <td>{item.unit}</td>
+          </tr>
+        );
       })
+      this.setState({ingredientsRow: this.state.ingredientsRow});
+    })
     api.randomUrl().then(res => {
       this.setState({url: res["url"][0]["url"]});
     })
@@ -101,35 +85,47 @@ export default class CreateDrinkView extends React.Component {
   render() {
     return (
       <section>
-      <RaisedButton
-        label="Create New Drink"
-        style={styles.floatingButton}
-        onClick={this.handleOpen}>
-      <ContentAdd/>
-      </RaisedButton>
-      <Dialog
-      open={this.state.open}
-      onRequestClose={this.handleClose}
-      bodyStyle={styles.dialog}>
-      <Card>
-        <CardHeader
-        title="Create New Drink"
-        />
-        <CardMedia>
-        <img style={styles.image} src={this.state.url}/>
-        </CardMedia>
-        <CardActions>
-        <FlatButton
-          label="Cancel"
-          primary={false}
-          keyboardFocused={false}
-          onTouchTap={this.handleClose}
-        />
-        </CardActions>
-        <br/>
-        {this.state.buttons}
-      </Card>
-      </Dialog>
+        <RaisedButton
+          label="Create New Drink"
+          style={styles.floatingButton}
+          onClick={this.handleOpen}>
+          <ContentAdd/>
+        </RaisedButton>
+        <Dialog
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+          bodyStyle={styles.dialog}>
+          <Card>
+            <CardHeader
+              title="Create New Drink"
+            />
+            <CardMedia>
+              <img style={styles.image} src={this.state.url}/>
+            </CardMedia>
+            <CardActions>
+              <FlatButton
+                label="Cancel"
+                primary={false}
+                keyboardFocused={false}
+                onTouchTap={this.handleClose}
+              />
+            </CardActions>
+            <CardText>
+              <table style={styles.table}>
+              <thead>
+                <tr>
+                  <th><h3>Name</h3></th>
+                  <th><h3>Amount</h3></th>
+                  <th><h3>Unit</h3></th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.ingredientsRow}
+              </tbody>
+              </table>
+            </CardText>
+          </Card>
+        </Dialog>
       </section>
     );
   }
